e2c2f9e99ac906a9b6eb9191089002fdc3e3bd1d
alessanderleite <alessander.logistica@gmail.com>
2019-04-12 11:23:41 -0300

    a little change on the json object structure inside the Post class

--- app/src/main/java/br/com/alessanderleite/getrequestretrofit2/Post.java
+++ app/src/main/java/br/com/alessanderleite/getrequestretrofit2/Post.java
@@ -4,58 +4,31 @@ import com.google.gson.annotations.SerializedName;
 
 public class Post {
 
-    @SerializedName("postId")
-    private String postId;
+    @SerializedName("userId")
+    private String userId;
 
     @SerializedName("id")
     private String id;
 
-    @SerializedName("name")
-    private String name;
-
-    @SerializedName("email")
-    private String email;
+    @SerializedName("title")
+    private String title;
 
     @SerializedName("body")
-    private String body;
+    private String text;
 
-    public String getPostId() {
-        return postId;
-    }
-
-    public void setPostId(String postId) {
-        this.postId = postId;
+    public String getUserId() {
+        return userId;
     }
 
     public String getId() {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public String getTitle() {
+        return title;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
+    public String getText() {
+        return text;
     }
 }
